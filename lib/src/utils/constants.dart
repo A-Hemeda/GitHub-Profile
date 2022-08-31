f9253f7655f0d21de7ee7a066236b8a32c97cce0
Ahmed Hemeda <101954795+A-Hemeda@users.noreply.github.com>
2022-08-31 18:45:04 +0200

    Delete lib directory

--- lib/src/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-  static const String ASSETS_IMAGES_PATH = 'assets/images/';
-}
--- lib/src/utils/my_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-const fPrimaryColor = Colors.yellow;
-const fCategoryColor = Colors.orangeAccent;
-
-const fSecondaryColor = Colors.blue;
-const fIconColor = Colors.grey;
-const fTextColor = Colors.grey;
